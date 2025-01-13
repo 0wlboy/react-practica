33b79a9e97b2273b4211eb1c7f68b1eab3d7584f
0wlboy <35744878+0wlboy@users.noreply.github.com>
2025-01-13 11:54:22 -0400

    updates

---
+++ practica-react/src/components/general/Button.jsx
@@ -0,0 +1,13 @@
+import propTypes from 'prop-types';
+
+function Button({texto ='presioname'}){
+  return(
+    <button>{texto}</button>
+  )
+}
+
+Button.propTypes ={
+  texto: propTypes.string
+}
+
+export default Button;
